0fd2a8613fdcd564ac0e8d851c667049b2595794
Facundo Montesanto <fmontesanto@gmail.com>
2018-10-24 11:20:55 -0300

    Agregue clase reserva + Controlador (incompleto)

--- src/com/Controller/Controlador.java
+++ src/com/Controller/Controlador.java
@@ -1,9 +1,19 @@
 package com.Controller;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import Hibernate.hibernateUtil;
+import dao.UsuarioDao;
+import excepciones.AlumnoException;
+import excepciones.ConnectionException;
+import excepciones.ProfesorException;
+import excepciones.UsuarioException;
+import negocio.Alumno;
+import negocio.Profesor;
+import negocio.Usuario;
 
 public class Controlador {
 
@@ -15,6 +25,53 @@ public class Controlador {
 		Session session = sf.openSession();
 		session.close();
 
+	}
+	
+	public void altaAlumno(String dni, String nombre, String mail, String telefono, String domicilio, Date fechaNacimiento,String password) throws ConnectionException,AlumnoException{
+		Alumno alumno=new Alumno(dni,nombre,mail,telefono,domicilio,fechaNacimiento,password);
+		//alumno.save();
+	}
+	public void modificarAlumno(String dni, String nombre, String mail, String telefono, String domicilio, Date fechaNacimiento,String password) throws ConnectionException, AlumnoException {
+		Alumno alumno=buscarAlumno(dni);
+		alumno.setNombre(nombre);
+		alumno.setMail(mail);
+		alumno.setTelefono(telefono);
+		alumno.setFechaNacimiento(fechaNacimiento);
+		alumno.setPassword(password);
+		//alumno.update();
+	}
+	public void bajaAlumno(String dni){
+		Alumno alumno=buscarAlumno(dni);
+		//alumno.delete();
+	}
+	private Alumno buscarAlumno(String dni) {
+		try {
+			Alumno alumno=AlumnoDao.getInstance().buscarAlumno(dni);
+		} catch (ConnectionException | AlumnoException e) {
+			e.printStackTrace();
+		}
+		return alumno;
+	}
+	
+	public void altaProfesor(String dni, String nombre, String mail, String telefono, String domicilio, Date fechaNacimiento,String password, String domicilioClases,float anticipacion) throws ConnectionException,ProfesorException{
+		Profesor profesor=new Profesor(domicilioClases, domicilioClases, domicilioClases, domicilioClases, domicilioClases, fechaNacimiento, domicilioClases, domicilioClases, anticipacion);
+		//profesor.save();
+	}
+	public void modificarProfesor(String dni, String nombre, String mail, String telefono, String domicilio, Date fechaNacimiento,String password, String domicilioClases,float anticipacion)throws ConnectionException,ProfesorException {
+		Profesor profesor=buscarProfesor(dni);
+	}
+	public void bajaProfesor(String dni) {
+		Profesor profesor=buscarProfesor(dni);
+		//profesor.delete();
+		
+	}
+	private Profesor buscarProfesor(String dni) {
+		try {
+			Profesor alumno=ProfesorDao.getInstance().buscarAlumno(dni);
+		} catch (ConnectionException | ProfesorException e) {
+			e.printStackTrace();
+		}
+		return profesor;
 	}
 
 }
--- src/negocio/Reserva.java
+++ src/negocio/Reserva.java
@@ -1,5 +1,86 @@
 package negocio;
 
+import java.util.ArrayList;
+
 public class Reserva {
+
+	private int codigo;
+	private float descuento;
+	private float monto;
+	private int cantAlumnos;
+	private float duracion;
+	private boolean paga;
+	private ArrayList<Clase> clases;
+	
+	public Reserva(int codigo, float descuento, float monto, int cantAlumnos, float duracion, boolean paga) {
+		super();
+		this.codigo = codigo;
+		this.descuento = descuento;
+		this.monto = monto;
+		this.cantAlumnos = cantAlumnos;
+		this.duracion = duracion;
+		this.paga = paga;
+		this.clases = new ArrayList<Clase>();
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public float getDescuento() {
+		return descuento;
+	}
+
+	public void setDescuento(float descuento) {
+		this.descuento = descuento;
+	}
+
+	public float getMonto() {
+		return monto;
+	}
+
+	public void setMonto(float monto) {
+		this.monto = monto;
+	}
+
+	public int getCantAlumnos() {
+		return cantAlumnos;
+	}
+
+	public void setCantAlumnos(int cantAlumnos) {
+		this.cantAlumnos = cantAlumnos;
+	}
+
+	public float getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(float duracion) {
+		this.duracion = duracion;
+	}
+
+	public boolean isPaga() {
+		return paga;
+	}
+
+	public void setPaga(boolean paga) {
+		this.paga = paga;
+	}
+
+	public ArrayList<Clase> getClases() {
+		return clases;
+	}
+
+	public void setClases(ArrayList<Clase> clases) {
+		this.clases = clases;
+	}
+	
+	public void agregarClase(Clase clase) {
+		clases.add(clase);
+	}
 
 }
