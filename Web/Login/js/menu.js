2506d44989352fbf127dff1804b2e55231d62481
fernandez-julian <julifersan@hotmail.com>
2018-11-14 08:19:39 -0300

    calendario profesor

--- Web/Login/js/menu.js
+++ Web/Login/js/menu.js
@@ -13,8 +13,29 @@ function validar(){
     alert("Completar contraseña");
   }
   else{
-   window.open("file:///C:/Users/Juli%C3%A1n/Desktop/Todo/Universidad/3er%20a%C3%B1o/Univ.2do%20cuatrimestre%202018/Seminario%20de%20integracion%20profesional%20I/Web/Alumno/idex.html");
-  }
-  
-};
+    var selectedValue = document.getElementById("log").value;
+    var a = "alu";
+    var b = "prof";
+    var c = "admin";
+   switch(selectedValue){
+           case a:
+           window.open("http://127.0.0.1:49923/Alumno/index.html");
+        break;
+          case b:
+            window.open("http://127.0.0.1:49708/Profesor/index.html");
+        break;
+          //case admin:
 
+  }
+      //window.open("file:///C:/Users/Juli%C3%A1n/Desktop/Todo/Universidad/3er%20a%C3%B1o/Univ.2do%20cuatrimestre%202018/Seminario%20de%20integracion%20profesional%20I/Web/Alumno/idex.html");
+
+  }
+
+  }
+
+   /*   function getSelectValue()
+    {
+        var selectedValue = document.getElementById("log").value;
+        console.log(selectedValue);
+    }
+    //getSelectValue();*/
