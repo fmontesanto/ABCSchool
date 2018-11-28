82237ef3d470a75ac6e007b826e0a6b8b6a75be8
Marco <mmercurio.c@gmail.com>
2018-11-28 11:47:27 -0300

    estructura final??

--- ProyectoWebAnterior/src/excepciones/ProfesorException.java
+++
@@ -1,9 +0,0 @@
-package excepciones;
-
-public class ProfesorException extends Exception {
-    private static final long serialVersionUID = 7911635775508174059L;
-
-    public ProfesorException(String mensaje) {
-        super(mensaje);
-    }
-}
