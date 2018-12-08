88f9eb68b4d01fbaf9f117d73c1114d2c0c007a9
fernandez-julian <julifersan@hotmail.com>
2018-12-08 18:59:07 -0300

    Alta alumno + perfil alumno

--- ABCSchool_Web/src/com/abcSchool/servlets/AltaAlumnoServlet.java
+++ ABCSchool_Web/src/com/abcSchool/servlets/AltaAlumnoServlet.java
@@ -6,7 +6,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import controller.Controlador;
+
+import delegates.BusinessDelegate;
 
 public class AltaAlumnoServlet extends HttpServlet {
 
@@ -18,7 +19,17 @@ public class AltaAlumnoServlet extends HttpServlet {
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String dni = request.getParameter("dni");
+		String nombre = request.getParameter("nombre");
+		String domicilio = request.getParameter("nombre");
+		String mail = request.getParameter("nombre");
+		String telefono = request.getParameter("nombre");
+		String fechaNac = request.getParameter("nombre");//Cambiar a Date!
+		String pass = request.getParameter("nombre");
+		String passConf = request.getParameter("nombre");
 		
+		BusinessDelegate.getInstancia().altaAlumno(dni, nombre, mail, telefono, domicilio, fechaNac, pass);
+		//metodo altaAlumno del Delegate, que devuelva boolean para verificar el alta del alumno
 	}
 	
 
